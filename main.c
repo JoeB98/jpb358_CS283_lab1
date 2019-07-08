4bf2ec72aa6da1ad5001f1147a8fedfc03172124
JoeB98 <ellisrambles@gmail.com>
2019-07-07 21:47:51 -0400

    reee
    asd

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello World\n");
+	int x = 55;
+	int *y = &x;
+	printf("Location of x is%p\n",y);
+    printf("Value of x is: %d\n",*y);
 	return 0;
 }
