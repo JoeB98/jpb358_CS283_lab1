bab0d9596a6dfea0ec13f8a549966620bfeba405
JoeB98 <ellisrambles@gmail.com>
2019-07-09 17:11:43 -0400

    Prob3

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 void problem1() {
-    int x = 5;
     int *array;
-    array = (int *)malloc(sizeof(int)*10);
+    array = (int*)malloc(sizeof(int)*10);
     for(int i=0; i<10; i++){
         array[i]=i+1;
         printf("%d\n",array[i]);
@@ -32,10 +31,39 @@ void problem2(){
     }
     return;
 }
+void sort(int size, int* a){
+    //ints to use for ordering stuff
+    int x, y, z;
+    for(x = 0; x < size; x++) {
+        for(y = x + 1; y < size; y++) {
+            //compares the dereferenced pointers to see which is smaller
+            if(*(a + y) < *(a + x)) {
+                //if the comparator passes a true set temp = to first val changed
+                z = *(a + x);
+                //set first val to second
+                *(a + x) = *(a + y);
+                //set second val to first
+                *(a + y) = z;
+            }
+        }
+    }
+    for(int i = 0; i < size; i++){
+        printf("%d\n", *(a + i));
+    }
+}
+void problem3(){
+    int size = 10;
+    int arr[] = {9, 6, 0, 5, 3, 7, 4, 8, 1, 2};
+    sort(size, arr);
+    return;
+}
+
 int main(){
     problem1();
     printf("\n");
     problem2();
+    printf("\n");
+    problem3();
     printf("\n");
     return 0;
 }
